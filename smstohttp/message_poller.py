e42f56d1724795cf048b1a043f3c5f6a9da8d9c0
Vivek Agarwal <nitkkr.vivek@gmail.com>
2015-08-30 04:40:51 +0530

    fix

--- smstohttp/message_poller.py
+++ smstohttp/message_poller.py
@@ -4,12 +4,16 @@ from urllib import urlencode
 import schedule, time
 import cjson
 import smstohttp
+import traceback
 
 user_name="nitkkr.vivek@gmail.com"
 password="123456789"
 device_id = "12382"
 
 def send_message(message, number):
+    print "Trying to send SMS now to"
+    print number
+    print message
     params = OrderedDict([
         ('email', user_name),
         ('password', password),
@@ -24,29 +28,41 @@ def send_message(message, number):
 
 def get_messages():
     params = OrderedDict([('email', user_name), ('password', password)])
+    print "Getting messages"
     try:
         content = requests.get('http://smsgateway.me/api/v3/messages', params=urlencode(params)).content
+        print "got messages"
         res = cjson.decode(content)
         if res["success"]:
             for each_res in res["result"]:
+                print "message recd"
                 if each_res["status"] == "received":
                     try:
-                        if time.time() - each_res["received_at"] > 60:
+                        print time.time() - 19800
+                        print each_res["received_at"]
+                        if time.time() - each_res["received_at"] - 43200> 420:
                             continue
+                        print each_res
+                        print "Qyering server"
                         message = smstohttp.main(each_res["message"])
+                        print "Response :"
+                        print message
                         if len(message) > 0:
-                            send_message(message, each_res["number"])
+                            print "Non zero len"
+                            send_message(message, each_res["contact"]["number"])
                         else:
-                            send_message("Sorry, we could not find any results :(", each_res["number"])
+                            print "zero len"
+                            send_message("Sorry, we could not find any results :(", each_res["contact"]["number"])
                     except Exception as e:
                         print "Error in handling"
+                        send_message("Something went wrong. We did not handle all exceptions, sorry :(", each_res["contact"]["number"])
                         print e
-
+                        traceback.print_exc()
 
     except requests.exceptions.RequestException as e:
         print e
 
-schedule.every(0.01).minutes.do(get_messages)
+schedule.every(1).minutes.do(get_messages)
 
 while True:
     schedule.run_pending()
