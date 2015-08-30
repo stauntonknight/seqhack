348b9c028a256e711def8e37abcf4f004390339b
Vai bhav <vaibhav.lists@gmail.com>
2015-08-30 09:39:26 +0530

    Modifications.

--- smstohttp/ui/view_page.js
+++ smstohttp/ui/view_page.js
@@ -1,21 +1,21 @@
 function delet(serviceName) {
-	$.get('http://5bcafef9.ngrok.io/api/services/deleteservice', {
-			'serviceName': serviceName
-			}, function() {
-				window.reload();
-			});
+    $.get('http://localhost:50467/api/services/DeleteService', {
+        serviceName: serviceName
+    }, function() {
+        window.reload();
+    });
 }
 
-$(document).ready(function() {
-	$.get('http://5bcafef9.ngrok.io/api/services/listservices', function(serviceNames) {
-		for (var i = 0 ; i < serviceNames.length; i++) {
-			var $el = $($('#hidden-template-servicename').html());	
-			$('#container').append($el);
-			$el.find(".servicename").text(serviceNames[i]);
-			var t = serviceNames[i];
-			$el.find(".delete").click(function() {
-				delet(t);
-			});
-		}
-	});
+$(document).ready(function () {
+    $.get('http://localhost:50467/api/services/listservices', function (serviceNames) {
+        for (var i = 0 ; i < serviceNames.length; i++) {
+            var $el = $($('#hidden-template-servicename').html());
+            $('#container').append($el);
+            $el.find(".servicename").text(serviceNames[i]);
+            var t = serviceNames[i];
+            $el.find(".delete").click(function () {
+                delet(t);
+            });
+        }
+    });
 });
