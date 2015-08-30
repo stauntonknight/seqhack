651aa2590539c907fb27bce24a18a862d72536e6
Shreyas Pandya <pandyashreyas1@gmail.com>
2015-08-30 12:11:35 +0530

    working code dont change

--- smstohttp/smstohttp.py
+++ smstohttp/smstohttp.py
@@ -5,17 +5,20 @@ import requests
 from Levenshtein import ratio
 from specializations import specializations
 from localities import localities
+from google_types import google_types
 
-service_end_point = 'http://e2630397.ngrok.io/api/Services/GetServiceDetails/'
-discovery_end_point = 'http://e2630397.ngrok.io/api/Services/GetApiDetails/'
+service_end_point = 'http://f644da1e.ngrok.io/api/Services/GetServiceDetails/'
+discovery_end_point = 'http://f644da1e.ngrok.io/api/Services/GetApiDetails/'
 keys = ['name', 'specialization']
-service_list = {'practo': 'Practo'}
-service_type_list = {'search': 'search'}
+service_list = {'practo': 'Practo', 'google': 'Google'}
+service_type_list = {'search': 'search', 'near': 'nearbySearch'}
 blacklist_words = set(["in", "from", "of"])
 
 param_corpus_dict = {
     'speciality': specializations,
-    'locality': localities
+    'locality': localities,
+    'location': localities,
+    'types': google_types
 }
 
 
@@ -61,6 +64,8 @@ def main(sms):
     if service_type is None:
         return ''
     service_type = service_type_list[service_type]
+    print service
+    print service_type
 
     api_spec_req = requests.get(
         discovery_end_point,
@@ -70,26 +75,48 @@ def main(sms):
     api_spec_req = api_spec_req.json()
 
     avail_params = {}
+    # api_spec_req = ['location', 'radius', 'types']
     for api_param in api_spec_req:
         if api_param in param_corpus_dict:
-            avail_params[api_param] = get_first_matching(
-                param_corpus_dict[api_param], tokens)
+            if api_param == 'location':
+                loca = get_first_matching(param_corpus_dict[api_param], tokens)
+                url = 'https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=AIzaSyDQT4UfphqtqYYYxGRsUjChUK6GeUDmLfY' % loca
+                lat_lng = requests.get(url).json()['results'][0]
+                location = lat_lng['geometry'][u'location']
+                avail_params[api_param] = str(location['lat']) + ',' + str(location['lng'])
+            else:
+                avail_params[api_param] = get_first_matching(
+                    param_corpus_dict[api_param], tokens)
+                print api_param
+                print get_first_matching(
+                    param_corpus_dict[api_param], tokens)
+    if 'radius' in api_spec_req:
+        avail_params['radius'] = 500
 
     if 'city' in api_spec_req:
         avail_params['city'] = 'Bangalore'
+
+    print avail_params
+    print api_spec_req
 
     if service is None or service_type is None:
         return ""
 
     resp = make_request(service, service_type, avail_params)
-    return resp_to_text(resp)
+    return resp_to_text(resp, service)
 
 
-def resp_to_text(resp):
-    if 'doctors' not in resp:
+def resp_to_text(resp, service):
+    if service=='Practo':
+        if 'doctors' not in resp:
+            return ''
+        resp = resp['doctors']
+    elif service=='Google':
+        resp = resp['results']
+    else:
         return ''
-    resp = resp['doctors']
-    keys = ['doctor_name', 'recommendation_percent', 'consultation_fees']
+
+    keys = ['doctor_name', 'recommendation_percent', 'consultation_fees', 'name', 'rating', 'vicinity']
     resp_sms = ''
     for item in resp[:3]:
         for key in keys:
