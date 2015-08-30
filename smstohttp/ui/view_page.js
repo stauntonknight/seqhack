724fab9dfd2667bc1428310ef68c15ad2eadbc0f
Vivek Agarwal <nitkkr.vivek@gmail.com>
2015-08-30 08:52:52 +0530

    f

--- smstohttp/ui/view_page.js
+++ smstohttp/ui/view_page.js
@@ -1,16 +1,18 @@
 function delet(serviceName) {
-	$.get('/deleteservice', serviceName, window.reload);
+	$.get('http://5bcafef9.ngrok.io/api/services/deleteservice', {
+			'serviceName': serviceName
+			}, window.reload);
 }
 
 $(document).ready(function() {
-	$.get('/listservice', function(serviceNames) {
+	$.get('http://5bcafef9.ngrok.io/api/services/listservices', function(serviceNames) {
 		for (var i = 0 ; i < serviceNames.length; i++) {
 			var $el = $($('#hidden-template-servicename').html());	
+			$('#container').append($el);
 			$el.find(".servicename").text(serviceNames[i]);
 			$el.find(".delete").click(function() {
 				delet(serviceNames[i]);
 			});
-			$('#container').append($el);
 		}
 	});
 });
